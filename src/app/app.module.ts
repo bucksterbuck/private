ca83db7b653537a5d15ead752f483d738e98abe6
Unknown <blake@workhorseart.com>
2018-01-26 17:26:34 -0500

    recommit
    recommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
@@ -10,31 +11,45 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import {RouterModule, Routes } from '@angular/router';
-import { AngularFireAuthModule, AngularFireAuthProvider, AngularFireAuth, AUTH_PROVIDERS } from 'angularfire2/auth';
+import { AngularFireAuthModule, AngularFireAuthProvider, } from 'angularfire2/auth';
 import { AuthService } from './services/auth.service';
+import {AuthGuardService} from './services/auth-guard.service';
+import { MembersComponent } from './members/members.component';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
 
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent}
-]
+  { path: 'login', component: LoginComponent},
+  { path: 'members', component: MembersComponent, canActivate: [AuthGuardService]}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase ),
     AngularFirestoreModule,
     AngularFireAuthModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
